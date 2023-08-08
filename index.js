d3ff270be81b4b04f1baf0509564302b53c45154
mikaela echo <mikaelaaecho@gmail.com>
2023-08-08 12:55:14 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+var num1 = 62
+var num2 = 1
+var multiply = 62 * 1 ;
 
-// Write your code here
+var random = Math.floor(Math.random() * 10) ;
+
+var num3 = 12
+var num4 = 3
+var mod = 12 / 3;
+
+var num5 = 10
+var num6 = 2
+var max = 10 * 2;
